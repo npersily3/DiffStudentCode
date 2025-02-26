a0c9c81fbfd44904f5d86203dce42fa9e6e4fd78
npersily1 <noah.persily@menloschool.org>
2025-02-26 10:32:45 -0800

    Worklog

--- src/PlagiarismChecker.java
+++ src/PlagiarismChecker.java
@@ -13,10 +13,16 @@ public class PlagiarismChecker {
      * @param doc1 the first document
      * @param doc2 the second
      * @return The length of the longest shared substring.
+     *
      */
-    public static int longestSharedSubstring(String doc1, String doc2) {
 
-        int[][] grid = new int[doc1.length()][doc2.length()];
+    public static int longestSharedSubstring(String doc1, String doc2) {
+        return lssLength(doc1,doc2);
+    }
+    private static int lssLength(String doc1, String doc2) {
+        int d1length = doc1.length();
+        int d2length = doc2.length();
+        int[][] grid = new int[d1length][d2length];
 
         char first = doc2.charAt(0);
         for (int i = 0; i < grid.length; i++) {
@@ -39,6 +45,63 @@ public class PlagiarismChecker {
             }
         }
 
-        return 0;
+
+        for (int i = 1; i < grid.length; i++) {
+            for (int j = 1; j < grid[0].length; j++) {
+                if(doc1.charAt(i) == doc2.charAt(j)) {
+                    grid[i][j] = grid[i-1][j-1] + 1;
+                }
+                else {
+                    grid[i][j] = Math.max(grid[i-1][j], grid[i][j-1]);
+                }
+            }
+        }
+
+        return grid[d1length - 1][d2length - 1];
+    }
+    private static String lssString(String doc1, String doc2) {
+        int d1length = doc1.length();
+        int d2length = doc2.length();
+        String[][] grid = new String[d1length][d2length];
+
+        char first = doc2.charAt(0);
+        for (int i = 0; i < grid.length; i++) {
+            if(first == doc1.charAt(i)) {
+                while (i < grid.length) {
+                    grid[i][0] = first + "";
+                    i++;
+                }
+                break;
+            }
+        }
+        first = doc1.charAt(0);
+        for (int i = 0; i < grid[0].length; i++) {
+            if(first == doc2.charAt(i)) {
+                while (i < grid[0].length) {
+                    grid[0][i] = first + "";
+                    i++;
+                }
+                break;
+            }
+        }
+
+
+        for (int i = 1; i < grid.length; i++) {
+            for (int j = 1; j < grid[0].length; j++) {
+                if(doc1.charAt(i) == doc2.charAt(j)) {
+                    grid[i][j] = grid[i-1][j-1] + doc1.charAt(i);
+                }
+                else {
+                    if(grid[i][j - 1].length() > grid[i-1][j].length()) {
+                        grid[i][j] = grid[i][j-1];
+                    }
+                    else {
+                        grid[i][j] = grid[i-1][j];
+                    }
+                }
+            }
+        }
+
+        return grid[d1length - 1][d2length - 1];
     }
 }
